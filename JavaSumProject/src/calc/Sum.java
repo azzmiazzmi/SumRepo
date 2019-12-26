df806cb32d3858d9e05539c799ee0274970b41f3
amalbakkour <amalbakkour@maz57.nu.edu.sa>
2019-12-27 01:00:01 +0300

    add sub method

--- JavaSumProject/src/calc/Sum.java
+++ JavaSumProject/src/calc/Sum.java
@@ -7,10 +7,15 @@ public class Sum {
 		return pFirstNumber + pSecondNumber;
 	}
 
+	public int sub (int pFirstNumber, int pSecondNumber){
+		return pFirstNumber - pSecondNumber;
+	}
+
+	
 	public static void main(String[] args) {
 	
 		System.out.println(new Sum().sum(2, 3));
-
+		System.out.println(new Sum().sum(5, 1));
 	}
 
 }
